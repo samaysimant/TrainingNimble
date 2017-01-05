c4fbcc4cabf10c612b9ccd00efa9a3f2d7e924a2
Sanjeev <Sanjeev@DESKTOP-OMR0JJR>
2017-01-05 08:58:55 +0530

    new code

--- SeleniumTrainingWithMaven/src/test/java/rawSelenium/RegisterNornamSeleniumActs.java
+++ SeleniumTrainingWithMaven/src/test/java/rawSelenium/RegisterNornamSeleniumActs.java
@@ -1,21 +1,42 @@
 package rawSelenium;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class RegisterNornamSeleniumActs extends BaseClass{
   @Test
   public void f() {
-	  
 	  driver.get("http://automationpractice.com/index.php");
+	  driver.findElement(By.xpath("//a[@class='login']")).click();
+	  driver.findElement(By.id("email_create")).sendKeys("jhbl1221@gmail.com");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.findElement(By.name("SubmitCreate")).click();
+	  driver.findElement(By.id("id_gender1")).click();
+	  driver.findElement(By.name("customer_firstname")).sendKeys("Ricky");
+	  driver.findElement(By.id("customer_lastname")).sendKeys("Pati");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.findElement(By.id("email")).clear();
+	  driver.findElement(By.id("email")).sendKeys("jhbl1221@gmail.com");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.findElement(By.id("passwd")).sendKeys("sandy123");
+	  driver.findElement(By.xpath("//select[@id='days']")).sendKeys("8");
+	  driver.findElement(By.id("months")).sendKeys("June");
+	  driver.findElement(By.id("years")).sendKeys("1988");
+	  driver.findElement(By.id("firstname")).sendKeys("Ricky");
+	  driver.findElement(By.id("lastname")).sendKeys("Pati");
+	  driver.findElement(By.id("address1")).sendKeys("Jefferson Road Rubi Layout");
+	  driver.findElement(By.id("city")).sendKeys("Bangalore");
+	  driver.findElement(By.id("id_state")).sendKeys("California");
+	  driver.findElement(By.id("postcode")).sendKeys("87656");
+	  
+	  driver.findElement(By.id("phone_mobile")).sendKeys("9878765653");
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.findElement(By.id("submitAccount")).click();
 	  
 	  
-	  //Click om Sign In
+	  Thread.sleep(30000);
 	  
-	  driver.findElement(By.cssSelector(".login")).click();
-	  
-	  driver.findElement(By.id("email_create")).sendKeys("test123samay@qwe.com");
-	  
-	  driver.findElement(By.cssSelector("button[type='submit'][name='SubmitCreate']")).click();
   }
 }
