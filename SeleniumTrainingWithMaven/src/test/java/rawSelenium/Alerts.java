27c7d5888cc83ef5422c8ed5fad95415ea8ac986
Samay Simant <sb00418692@techmahindra.com>
2017-01-06 09:23:55 +0530

    new2

--- SeleniumTrainingWithMaven/src/test/java/rawSelenium/Alerts.java
+++ SeleniumTrainingWithMaven/src/test/java/rawSelenium/Alerts.java
@@ -1,19 +1,7 @@
 package rawSelenium;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Alerts extends BaseClass{
